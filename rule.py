2b50584d84a0f91ee1e019540293c530bf06227f
cfstyle <702108215@qq.com>
2018-09-03 11:56:58 +0800

    update rule

--- rule.py
+++ rule.py
@@ -6,11 +6,12 @@
 '''
 
 RULES = {
-    '美食': ['美食', 'food', 'recipe'],
-    '壁纸': ['壁纸', 'wallpaper', 'background image'],
-    '咖啡': ['咖啡', 'coffee'],
+    '美食': ['美食', 'food', 'recipe', 'cate', 'cuisine', 'buffet', 'fast food', 'specialty', 'delicious', 'yummy'],
+    '壁纸': ['壁纸', 'wallpaper', 'background image', ''],
+    '咖啡': ['咖啡', 'coffee', 'espresso', 'cafe', 'latte', 'cappuccino', 'caffe mocha', 'macchiatto'],
     '汽车': ['汽车', 'car', 'vehicle',  'roadster', 'sportscar'],
-    '设计': ['设计', 'design', 'designer'],
+    '设计': ['设计', 'design', 'designer', 'graphic design'],
     '植物': ['植物', 'plant', 'flora', 'flowers', 'plants'],
     '健身': ['健身', 'fitness', 'gym', 'bodybuilding', 'yoga', 'press', 'squat', 'work out'],
+    '动物': ['动物', '宠物', 'animal', 'pet']
 }

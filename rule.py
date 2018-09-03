28e7be56c09477ecd31730e3d11baba3ce2372f7
cfstyle <702108215@qq.com>
2018-09-03 10:38:35 +0800

    add rules.p

---
+++ rule.py
@@ -0,0 +1,16 @@
+#encoding=utf-8
+'''
+@author: chenfei
+@time: 2018-9-3 10:23:10
+@description: rules for scrapy
+'''
+
+RULES = {
+    '美食': ['美食', 'food', 'recipe']
+    '壁纸': ['壁纸', 'wallpaper', 'background image']
+    '咖啡': ['咖啡', 'coffee'],
+    '汽车': ['汽车', 'car', 'vehicle',  'roadster', 'sportscar'],
+    '设计': ['设计', 'design', 'designer'],
+    '植物': ['植物', 'plant', 'flora', 'flowers', 'plants'],
+    '健身': ['健身', 'fitness', 'gym', 'bodybuilding', 'yoga', 'press', 'squat', 'work out'],
+}
